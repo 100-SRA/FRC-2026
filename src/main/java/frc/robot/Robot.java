ab71e27334b1b30b7ba98d5c1ec2c627ba6fe168
Julius L. Jones Jr. <juliuslwj22@gmail.com>
2026-03-14 08:50:16 -0400

    fixed build

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -26,7 +26,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    CameraServer.startAutomaticCapture(); // Actvate the webcam server
+    // CameraServer.startAutomaticCapture(); // Activate the webcam server (not yet configured)
   }
 
   /**
